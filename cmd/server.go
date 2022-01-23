7324b40fa30e5c3205a3b1d824ed17eda6cc5302
dmitry-bakeev <bakeevdimon@gmail.com>
2022-01-23 12:57:17 +0500

    Add server logics

--- cmd/server.go
+++ cmd/server.go
@@ -5,9 +5,13 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -16,12 +20,7 @@ import (
 var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  "",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		host, err := cmd.Flags().GetString("host")
 		if err != nil {
@@ -43,15 +42,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(serverCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serverCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	serverCmd.Flags().StringP("host", "H", "localhost", "Set listeting host")
 	serverCmd.Flags().IntP("port", "p", 3001, "Set listeting port")
 }
@@ -61,18 +51,120 @@ type App struct {
 	Port int
 }
 
+type Rate struct {
+	Symbol string `json:"symbol"`
+	Price  string `json:"price"`
+}
+
+type PostJSON struct {
+	Pairs []string `json:"pairs"`
+}
+
+type RateSlice []Rate
+
+const API_URL = "https://api.binance.com/api/v3/ticker/price"
+
+func GetCurrentRates() (RateSlice, error) {
+	response, err := http.Get(API_URL)
+	if err != nil {
+		return RateSlice{}, err
+	}
+	defer response.Body.Close()
+
+	var curretRates RateSlice
+	err = json.NewDecoder(response.Body).Decode(&curretRates)
+	if err != nil {
+		return RateSlice{}, err
+	}
+
+	return curretRates, nil
+}
+
+func FindRates(pairs []string) (RateSlice, error) {
+	var result RateSlice
+
+	curretRates, err := GetCurrentRates()
+	if err != nil {
+		return RateSlice{}, err
+	}
+
+	for _, pair := range pairs {
+		for _, currentRate := range curretRates {
+			tmp := strings.Replace(pair, "-", "", -1)
+			if tmp == currentRate.Symbol {
+				result = append(result, Rate{pair, currentRate.Price})
+			}
+		}
+	}
+
+	return result, nil
+}
+
+func RatesToJSON(rates RateSlice) ([]byte, error) {
+	rateMap := make(map[string]float64)
+	for _, rate := range rates {
+		tmp, err := strconv.ParseFloat(rate.Price, 64)
+		if err != nil {
+			return []byte{}, err
+		}
+		rateMap[rate.Symbol] = tmp
+	}
+
+	result, err := json.Marshal(rateMap)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return result, nil
+}
+
+func IntermediateHandler(rw http.ResponseWriter, r *http.Request, pairs []string) {
+	rates, err := FindRates(pairs)
+	if err != nil {
+		http.Error(rw, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	result, err := RatesToJSON(rates)
+	if err != nil {
+		http.Error(rw, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(result)
+}
+
 func RootHandler(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		rw.WriteHeader(http.StatusOK)
-		fmt.Fprintf(rw, "OK!\n")
+		pairsStr := r.URL.Query().Get("pairs")
+		if pairsStr == "" {
+			http.Error(rw, "Bad request", http.StatusBadRequest)
+			return
+		}
+		pairs := strings.Split(pairsStr, ",")
+		IntermediateHandler(rw, r, pairs)
+	case http.MethodPost:
+		receivedJSON, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		var postJSON PostJSON
+		err = json.Unmarshal(receivedJSON, &postJSON)
+		if err != nil {
+			http.Error(rw, "Bad request", http.StatusBadRequest)
+			return
+		}
+		IntermediateHandler(rw, r, postJSON.Pairs)
 	default:
 		http.Error(rw, "Method not allowed", http.StatusMethodNotAllowed)
 	}
-
 }
 
 func startServer(a App) {
-	http.HandleFunc("/", RootHandler)
+	http.HandleFunc("/api/v1/rates", RootHandler)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", a.Host, a.Port), nil)
 }
